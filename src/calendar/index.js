680cd37c513b50c46c2148c4181179a1314dcbd6
Mariano <mariano.burgos@pjn.gov.ar>
2022-09-21 10:04:50 -0300

    Commit con las primeras 10 clases de la seccion 22

---
+++ src/calendar/index.js
@@ -0,0 +1,5 @@
+export * from './components/CalendarModal';
+export * from './components/CalendarEvent';
+export * from './components/Navbar';
+
+export * from './pages/CalendarPage';
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, RouterProvider, Routes } from "react-router-dom"
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+
+//const authStatus = 'not-authenticated';
+const authStatus = 'authenticated';
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      {
+        (authStatus === 'not-authenticated') ?
+          <Route path="/auth/*" element={<LoginPage />} />
+          : <Route path="/*" element={<CalendarPage />} />
+
+      }
+
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
+    </Routes>
+  )
+}
