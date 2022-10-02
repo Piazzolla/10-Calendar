6bff79959b1daa672f441d7248b7437c4fab8361
Mariano <macacozzz@hotmail.com>
2022-10-02 19:23:06 -0300

    tests en authSlice

---
+++ tests/fixtures/testUser.js
@@ -0,0 +1,6 @@
+export const testUserCredentials = {
+    email: 'test@google.com',
+    password: '123456',
+    uid: '63376250bc11906f5a3e3164',
+    name: 'Test User'
+}
---
+++ tests/store/auth/authSlice.test.js
@@ -0,0 +1,9 @@
+import { authSlice } from "../../../src/store/auth/authSlice"
+import { initialState } from "../../fixtures/authStates"
+
+describe('Pruebas en authSlice', () => { 
+    test('debe regresar el estado inicial', () => { 
+        expect( authSlice.getInitialState()).toEqual(initialState);
+
+     })
+ })
